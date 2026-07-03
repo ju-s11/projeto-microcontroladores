477bd80b07f1510e3374b014a51cffc96c81e7bd
manuborba <manunapuc25@gmail.com>
2026-07-03 20:00:30 -0300

    Uncomment Arduino command sending functionality

--- arduino.py
+++ arduino.py
@@ -5,8 +5,8 @@ arduino = serial.Serial('COM6', 9600) # Se atentar sobre essa porta COM6
 time.sleep(2)
 
 def enviar(comando):
-    print(f"[ENVIARIA PRO ARDUINO]: {comando}")
-    # arduino.write((comando + '\n').encode())   # descomenta quando tiver o Arduino
+    #print(f"[ENVIARIA PRO ARDUINO]: {comando}")
+    arduino.write((comando + '\n').encode())   # descomenta quando tiver o Arduino
 
 def ler():
     if arduino.in_waiting > 0:
@@ -16,20 +16,3 @@ def ler():
 def fechar():
     arduino.close()
 
-SERVOS = {"prisma": 1, "foguete": 2, "lua": 3}
-
-LUZES = {
-    "OFF": "apagar",
-    "Branco": "branco",
-    "Fogo": "fogo",
-    "Espectro": "espectro",
-    "Dispersão de luz branca": "dispersao",
-    "Cálculo em loop": "calculo",
-    "Fluxo de dados": "dados",
-    "Decolagem do foguete": "decolagem",
-    "Respirar": "respirar"
-}
-
-def comando_luz(nome_luz):
-    efeito = LUZES[nome_luz]
-    return f"led {efeito} 40"
